c5dd2cb0f8ad35aac6fe0a14f4ac6a5407ec3aa6
BarryMonkingWang <100853574+BarryMonkingWang@users.noreply.github.com>
2023-09-14 15:01:06 +0800

    Update HelloControllerTest.cs

--- AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
+++ AspNetCoreWebServiceTest/Controllers/HelloControllerTest.cs
@@ -10,6 +10,7 @@ namespace AspNetCoreWebServiceTest.Controllers
         {
             HelloController controller = new HelloController();
             var response = controller.Get().Value as Response;
+            var content = response.Content.ReadAsStringAsync().Result;
             Assert.Equal("Hello World!", content);
         }
 
